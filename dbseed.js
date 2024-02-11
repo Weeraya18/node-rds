3ba69ddc91670df944b603ceff49621ceb737959
Weeraya18 <157940302+Weeraya18@users.noreply.github.com>
2024-02-11 16:55:37 +0700

    completed #1

---
+++ dbseed.js
@@ -0,0 +1,21 @@
+const mysql = require('mysql');
+
+const con = mysql.createConnection({
+    host: "database-pat.cpik6822ykjd.us-east-1.rds.amazonaws.com",
+    user: "admin",
+    password: "KaYhnhtV2oGojLLbpgl3"
+});
+
+
+
+con.connect(function(err) {
+    if (err) throw err;
+
+    con.query('CREATE DATABASE IF NOT EXISTS main;');
+    con.query('USE main;');
+    con.query('CREATE TABLE IF NOT EXISTS users(id int NOT NULL AUTO_INCREMENT, username varchar(30), '+
+    'email varchar(255), age int, PRIMARY KEY(id));', function(error, result, fields) {
+        console.log(result);
+    });
+    con.end();
+});
